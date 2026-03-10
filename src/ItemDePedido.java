cfd237567e5e7dbd313840e7b25d2440a1816880
Gabriel Henrique Silva Pereira <676690@sga.pucminas.br>
2026-03-09 22:27:46 -0300

    atividade avaliativa

---
+++ src/ItemDePedido.java
@@ -0,0 +1,54 @@
+public class ItemDePedido {
+
+    // Atributos encapsulados
+    private Produto produto;
+    private int quantidade;
+    private double precoVenda;
+
+    /**
+     * Construtor da classe ItemDePedido.
+     * O precoVenda deve ser capturado do produto no momento da criação do item,
+     * garantindo que alterações futuras no preço do produto não afetem este pedido.
+     */
+    public ItemDePedido(Produto produto, int quantidade, double precoVenda) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+        this.precoVenda = precoVenda;
+    }
+
+    public double calcularSubtotal() {
+        return 0;
+    }
+
+    // --- Sobrescrita do método equals ---
+
+    /**
+     * Compara a igualdade entre dois itens de pedido.
+     * A regra de negócio define que dois itens são iguais se possuírem o mesmo Produto.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        ItemDePedido outro = (ItemDePedido)obj;
+        return this.produto.equals(outro.produto);
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPrecoVenda() {
+        return precoVenda;
+    }
+}
